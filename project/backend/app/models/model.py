2eddc421940c0944a2b04b207b8ac3e3af5bbb66
kkoouuya <koya.iwabuchi@rakus-partners.co.jp>
2022-02-15 13:26:30 +0900

    change database model

--- project/backend/app/models/model.py
+++ project/backend/app/models/model.py
@@ -7,19 +7,6 @@ from datetime import datetime
 
 Base = declarative_base()
 
-class UserSite(Base):
-  __tablename__ = "users_sites"
-  
-  user_id = Column(UUIDType(binary=False), ForeignKey('users.user_id'), primary_key=True)
-  site_id = Column(UUIDType(binary=False), ForeignKey('sites.site_id'), primary_key=True)
-  is_deleted = Column(Integer, default=0, nullable=False)
-  version = Column(Integer, default=0, autoincrement=True, nullable=False)
-  created_at = Column(DateTime, default=datetime.now().strftime("%Y-%m-%d %H:%M:%S"), nullable=False)
-  updated_at = Column(DateTime, server_default=text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'), nullable=False)
-  
-  user = relationship('User')
-  site = relationship('Site')
-
 
 class User(Base):
   __tablename__ = "users"
@@ -32,31 +19,18 @@ class User(Base):
   version = Column(Integer, default=0, autoincrement=True, nullable=False)
   created_at = Column(DateTime, default=datetime.now().strftime("%Y-%m-%d %H:%M:%S"), nullable=False)
   updated_at = Column(DateTime, server_default=text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'), nullable=False)
-  
-  sites = relationship(
-    'Site',
-    secondary=UserSite.__tablename__,
-    back_populates='users',
-  )
-  user_site = relationship('UserSite')
+  site = relationship('Site', backref='user')
   
   
 class Site(Base):
   __tablename__ = "sites"
   
   site_id = Column(UUIDType(binary=False), primary_key=True, default=uuid.uuid4)
-  sitename = Column(String(512), nullable=False)
-  sitelink = Column(String(512), nullable=False)
+  user_id = Column(UUIDType(binary=False), ForeignKey('users.user_id',onupdate='CASCADE', ondelete='CASCADE'))
+  title = Column(String(512), nullable=False)
+  url = Column(String(512), nullable=False)
   word = Column(String(512), nullable=False)
   is_deleted = Column(Integer, default=0, nullable=False)
   version = Column(Integer, default=0, autoincrement=True, nullable=False)
   created_at = Column(DateTime, default=datetime.now().strftime("%Y-%m-%d %H:%M:%S"), nullable=False)
   updated_at = Column(DateTime, server_default=text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'), nullable=False)
-  
-  users = relationship(
-    'User',
-    secondary=UserSite.__tablename__,
-    back_populates='sites',
-  )
-  
-  user_site = relationship('UserSite')
